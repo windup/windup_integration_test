5842421d3409b986da65b791e24ec9e4f9374ea2
Nikhil Dhandre <ndhandre@redhat.com>
2020-03-09 19:18:33 +0530

    Added ipython shell for RHAMT

--- rhamt/scripting/cli.py
+++ rhamt/scripting/cli.py
@@ -1,11 +1,15 @@
 import click
 
+from rhamt.scripting.shell import main as shell
+
 
 @click.group()
 def cli():
     """CLI Tool for RHAMT"""
     pass
 
+
+cli.add_command(shell, name="shell")
 
 if __name__ == "__main__":
     cli()
