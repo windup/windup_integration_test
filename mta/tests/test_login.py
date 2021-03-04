096609581260f7cef46271f62825df230536e067
Shveta <sshveta@localhost.localdomain>
2021-03-03 20:20:44 -0800

    Operator automation

--- mta/tests/test_login.py
+++ mta/tests/test_login.py
@@ -2,6 +2,7 @@ import pytest
 
 from mta.base.application.implementations.web_ui import navigate_to
 
+
 @pytest.mark.smoke
 @pytest.mark.parametrize("mta_app", ["ViaWebUI", "ViaOperatorUI"], indirect=True)
 def test_login(mta_app):
