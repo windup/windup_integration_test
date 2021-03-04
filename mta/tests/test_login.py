9444b729d33b183a02de4bab7f37b3490f3ca9b8
Shveta <sshveta@localhost.localdomain>
2021-03-03 20:15:38 -0800

    Operator automation

--- mta/tests/test_login.py
+++ mta/tests/test_login.py
@@ -1,11 +1,26 @@
 import pytest
 
 from mta.base.application.implementations.web_ui import navigate_to
-
+from mta.base.application.implementations.operator_ui import ViaOperatorUI
 
 @pytest.mark.smoke
 @pytest.mark.parametrize("mta_app", ["ViaWebUI", "ViaOperatorUI"], indirect=True)
-def test_login(application, mta_app):
-    """Test login nav destination"""
-    view = navigate_to(application.collections.base, "LoggedIn")
+def test_login(mta_app):
+    """Test login nav destination
+
+    Polarion:
+        assignee: ghubale
+        initialEstimate: 1/12h
+        caseimportance: medium
+        caseposneg: positive
+        testtype: smoke
+        casecomponent: WebConsole
+        testSteps:
+            1. Login to MTA project
+        expectedResults:
+            1. It should successfully navigate to project all page or
+               Welcome to the Migration Toolkit for Applications page
+    """
+
+    view = navigate_to(mta_app.collections.base, "LoggedIn")
     assert view.is_displayed
