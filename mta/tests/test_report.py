d7da03c9c78bcb5ed3252108f12480584aa3a01a
Sachin Singla <sachinsingla321@gmail.com>
2022-09-21 16:10:53 +0530

    fixed some cases

--- mta/tests/test_report.py
+++ mta/tests/test_report.py
@@ -30,7 +30,7 @@ def test_send_feedback_and_validate_url(mta_app, create_minimal_project, request
     view.wait_displayed()
     view.analysis_results.show_report(request)
     # Validate URL report has mta-ui/api/static-report
-    url = "/mta-ui/api/static-report"
+    url = "/windup-ui/api/static-report"
     assert url in mta_app.web_ui.widgetastic_browser.url
     view = project_collection.create_view(AllApplicationsView)
     view.send_feedback.click()
@@ -93,14 +93,14 @@ def test_sort_application_list(mta_app, create_project, request):
     app_list_by_story_points = view.application_table.get_applications_list
     app_list_story_points_desc = [
         "bw-note-ear-4.0.0.ear",
-        "acmeair-webapp-1.0-SNAPSHOT.war",
         "cadmium-war-0.1.0.war",
+        "acmeair-webapp-1.0-SNAPSHOT.war",
     ]
     assert app_list_by_story_points[:-1] == app_list_story_points_desc
     view.alpha_sort.click()
     app_list_story_points_asc = [
-        "cadmium-war-0.1.0.war",
         "acmeair-webapp-1.0-SNAPSHOT.war",
+        "cadmium-war-0.1.0.war",
         "bw-note-ear-4.0.0.ear",
     ]
     app_list_by_story_points = view.application_table.get_applications_list
